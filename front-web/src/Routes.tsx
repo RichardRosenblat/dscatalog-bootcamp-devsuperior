34b25a35cdc602e73162947924daafd9a85ecba5
Richard <rosenblatr@gmail.com>
2020-10-11 16:56:16 -0300

    Routes Bug

---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Admin from './pages/Admin';
+import Catalog from './pages/Catalog';
+import Home from './pages/Home';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/">
+                <Home />
+            </Route>
+            <Route path="/catalog">
+                <Catalog />
+            </Route>
+            <Route path="/admin">
+                <Admin />
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
---
+++ front-web/src/pages/Admin/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Admin = () => (
+    <div>
+        <h1>ADMIN</h1>
+    </div>
+);
+
+export default Admin;

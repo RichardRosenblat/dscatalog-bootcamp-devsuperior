d442aff450417afd6d98e1da91aa0bfcc72294d1
Richard <rosenblatr@gmail.com>
2020-11-23 13:34:32 -0300

    integrate product listing with backend

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import Pagination from 'core/components/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import Card from "../Card";
 
 const List = () => {
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading,setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
+
+    console.log(productsResponse);
+
+    useEffect(() => {
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+        .then(response=>setProductsResponse(response.data))
+        .finally(() => {
+            setIsLoading(false);
+        })
+    }, [activePage]);
+
     const history = useHistory();
 
     const handleCreate = () => {
@@ -15,9 +37,18 @@ const List = () => {
                 ADICIONAR
             </button>
             <div className="admin-list-container">
-                <Card/>
-                <Card/>
-                <Card/>
+                {productsResponse?.content.map(product => (
+                    <Card product={product}  key={product.id}/>
+                ))}
+
+                {productsResponse && (
+                <Pagination 
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />
+
+            )}
             </div>
         </div>
     )

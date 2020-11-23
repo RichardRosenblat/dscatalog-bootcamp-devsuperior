a2915562d892c6cb8641f4c6745760afc847864d
Richard <rosenblatr@gmail.com>
2020-11-23 16:44:23 -0300

    Edit ptoduct

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -10,12 +10,12 @@ const List = () => {
     const [isLoading,setIsLoading] = useState(false);
     const [activePage, setActivePage] = useState(0);
 
-    console.log(productsResponse);
-
     useEffect(() => {
         const params = {
             page: activePage,
-            linesPerPage: 4
+            linesPerPage: 4,
+            direction: 'DESC',
+            orderBy: 'id'
         }
         setIsLoading(true);
         makeRequest({ url: '/products', params })

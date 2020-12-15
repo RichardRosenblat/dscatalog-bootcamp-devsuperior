6fbee531bcc2d4ad2efc1491688a58aaa9338ab5
Richard <rosenblatr@gmail.com>
2020-12-15 14:59:20 -0300

    add list unit test

---
+++ front-web/src/core/utils/__tests__/list.ts
@@ -0,0 +1,24 @@
+import {generateList} from '../list';
+
+test('should generate a list', () => {
+    // ARRANGE
+    const amount = 5;
+
+    // ACT
+    const result = generateList(amount);
+
+    //ASSERT
+    expect(result).toEqual([0, 1, 2, 3, 4]);
+})
+
+
+test('should generate an empty list when amount is zero', () => {
+    // ARRANGE
+    const amount = 0;
+
+    // ACT
+    const result = generateList(amount);
+
+    //ASSERT
+    expect(result).toEqual([]);
+})
